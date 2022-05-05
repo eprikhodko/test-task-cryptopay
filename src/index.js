a13672cc64d0827da805a790b281fdac6e21a033
eprikhodko <eprikhodkodev@gmail.com>
2022-05-06 00:11:22 +0300

    feat: make mobile navigation close on cross icon click

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import './scss/main.scss';
 console.log('hello from main page');
 
 // code for burger menu
-// const closeButton = document.querySelector('.header-nav__button-close-nav');
 const openButton = document.querySelector('.header__button-open-nav');
+const closeButton = document.querySelector('.sidebar__button-close-nav');
 const nav = document.querySelector('.sidebar');
 const body = document.querySelector('body');
 
-// closeButton.addEventListener('click', () => {
-//   nav.classList.remove('navigation-open');
-//   body.classList.remove('disable-scroll');
-// });
-
 openButton.addEventListener('click', () => {
-  nav.classList.toggle('navigation-open');
-  body.classList.toggle('disable-scroll');
-  openButton.classList.toggle('header-nav__button-open-nav_rotate');
-  overlayDark.classList.toggle('bg-semi-transparent');
+  nav.classList.add('navigation-open');
+  body.classList.add('disable-scroll');
+});
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('navigation-open');
+  body.classList.remove('disable-scroll');
 });
