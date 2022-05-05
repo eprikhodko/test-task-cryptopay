0cbbdf62f78bcf68e25788e873e7728ec78355c1
eprikhodko <eprikhodkodev@gmail.com>
2022-05-06 00:16:44 +0300

    feat: finish styles for the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './scss/main.scss';
 
-console.log('hello from main page');
-
 // code for burger menu
 const openButton = document.querySelector('.header__button-open-nav');
 const closeButton = document.querySelector('.sidebar__button-close-nav');
 const nav = document.querySelector('.sidebar');
+const navItems = document.querySelectorAll('.navigation__item');
 const body = document.querySelector('body');
 
 openButton.addEventListener('click', () => {
@@ -16,4 +15,11 @@ openButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   nav.classList.remove('navigation-open');
   body.classList.remove('disable-scroll');
+});
+
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('navigation-open');
+    body.classList.remove('disable-scroll');
+  });
 });
