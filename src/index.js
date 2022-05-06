f3c76a6376250c4f0556a6a220c76755e60c37e8
eprikhodko <eprikhodkodev@gmail.com>
2022-05-06 10:45:03 +0300

    feat: fetch currencies data from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import './scss/main.scss';
 
+// fetch data from API
+
+const url =
+  'https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency';
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let currencies = data.data;
+    console.log(currencies);
+    // console.log(currencies.data);
+
+    currencies.map((item) => {
+      console.log(item);
+    });
+  })
+  .catch(function (error) {
+    console.log(error, 'error');
+  });
+
 // code for burger menu
 const openButton = document.querySelector('.header__button-open-nav');
 const closeButton = document.querySelector('.sidebar__button-close-nav');
