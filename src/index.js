2dabe24f830f7b6ac4311ef17372e321326c3be0
eprikhodko <eprikhodkodev@gmail.com>
2022-05-06 11:02:29 +0300

    feat: fetch currencies data using async await

--- src/index.js
+++ src/index.js
@@ -2,25 +2,45 @@ import './scss/main.scss';
 
 // fetch data from API
 
-const url =
-  'https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency';
+// const url =
+//   'https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency';
 
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let currencies = data.data;
-    console.log(currencies);
-    // console.log(currencies.data);
+// fetch(url)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     let currencies = data.data;
+//     console.log(currencies);
+//     // console.log(currencies.data);
 
-    currencies.map((item) => {
-      console.log(item);
-    });
-  })
-  .catch(function (error) {
+//     currencies.map((item) => {
+//       console.log(item);
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error, 'error');
+//   });
+
+const fetchCurrenciesData = async () => {
+  try {
+    const res = await fetch(
+      'https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency'
+    );
+    const json = await res.json();
+    // console.log(json.data);
+    return json.data;
+  } catch (err) {
     console.log(error, 'error');
-  });
+  }
+};
+
+const mapCurrencies = async () => {
+  const currenciesData = await fetchCurrenciesData();
+  console.log(currenciesData);
+};
+
+mapCurrencies();
 
 // code for burger menu
 const openButton = document.querySelector('.header__button-open-nav');
