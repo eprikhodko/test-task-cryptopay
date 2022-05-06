50bece9870321fc4dc4dfc2f63d43c6255421686
eprikhodko <eprikhodkodev@gmail.com>
2022-05-06 12:42:20 +0300

    refactor: clean up js code for page

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const fetchCurrenciesData = async () => {
       'https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency'
     );
     const json = await res.json();
-    // console.log(json.data);
     return json.data;
   } catch (err) {
     console.log(error, 'error');
@@ -17,75 +16,63 @@ const fetchCurrenciesData = async () => {
 
 const mapCurrencies = async () => {
   const currenciesData = await fetchCurrenciesData();
-  //   console.log(currenciesData);
   currenciesData.sort(function (a, b) {
     return b.rate - a.rate;
   });
 
-  const createCard = (currencyData) => {
-    // console.log(currencyData);
-    const createCardTemplate = () => {
-      const card = document.createElement('div');
-      card.classList.add('card');
+  const createCardTemplate = (currencyData) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-      const cardCurrency = document.createElement('div');
-      cardCurrency.classList.add('card__currency');
+    const cardCurrency = document.createElement('div');
+    cardCurrency.classList.add('card__currency');
+    card.append(cardCurrency);
 
-      const cardCurrencyIcon = document.createElement('span');
-      cardCurrencyIcon.classList.add('ico');
-      cardCurrencyIcon.classList.add('ico_etherium');
+    const cardCurrencyIcon = document.createElement('span');
+    cardCurrencyIcon.classList.add('ico');
+    cardCurrencyIcon.classList.add('ico_etherium');
+    cardCurrency.append(cardCurrencyIcon);
 
-      const cardWrapper = document.createElement('div');
-      cardWrapper.classList.add('card__wrapper');
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card__wrapper');
+    cardCurrency.append(cardWrapper);
 
-      const cardRate = document.createElement('p');
-      cardRate.classList.add('card__rate');
-      cardRate.innerHTML = `${currencyData.rate}`;
+    const cardRate = document.createElement('p');
+    cardRate.classList.add('card__rate');
+    cardRate.innerHTML = `${currencyData.rate}`;
+    card.append(cardRate);
 
-      cardCurrency.append(cardCurrencyIcon);
-      cardCurrency.append(cardWrapper);
-      card.append(cardCurrency);
-      card.append(cardRate);
+    const cardTitle = document.createElement('h2');
+    cardTitle.innerHTML = `${
+      currencyData.fullName
+        ? currencyData.fullName
+        : currencyData.pair.slice(0, -4)
+    }`;
+    cardTitle.classList.add('card__title');
+    cardWrapper.append(cardTitle);
 
-      const cardTitle = document.createElement('h2');
-      cardTitle.innerHTML = `${
-        currencyData.fullName
-          ? currencyData.fullName
-          : currencyData.pair.slice(0, -4)
-      }`;
-      cardTitle.classList.add('card__title');
-      cardWrapper.append(cardTitle);
-
-      const cardSubtitle = document.createElement('h3');
-      cardSubtitle.innerHTML = `${currencyData.symbol}`;
-      cardSubtitle.classList.add('card__subtitle');
-      cardWrapper.append(cardSubtitle);
-
-      return card;
-    };
-
-    // create currency card
-    const card = createCardTemplate();
+    const cardSubtitle = document.createElement('h3');
+    cardSubtitle.innerHTML = `${currencyData.symbol}`;
+    cardSubtitle.classList.add('card__subtitle');
+    cardWrapper.append(cardSubtitle);
 
     return card;
   };
 
-  const setCards = (currencyData) => {
-    const card = createCard(currencyData);
+  const setCard = (currencyData) => {
+    const card = createCardTemplate(currencyData);
 
     CARDS.append(card);
   };
 
   // create cards elements
   for (let i = 0; i < currenciesData.length; i += 1) {
-    setCards(currenciesData[i]);
+    setCard(currenciesData[i]);
   }
 
   // code for sort by filter
   const SORT_BY = document.querySelector('.sort-by__value');
   const SORT_BY_ARROW_ICON = document.querySelector('.ico_arrow');
-
-  //   .ico_arrow-reverse
 
   SORT_BY.addEventListener('click', () => {
     SORT_BY_ARROW_ICON.classList.toggle('ico_arrow-reverse');
@@ -94,7 +81,7 @@ const mapCurrencies = async () => {
     CARDS.innerHTML = '';
 
     for (let i = 0; i < reverseArray.length; i += 1) {
-      setCards(reverseArray[i]);
+      setCard(reverseArray[i]);
     }
   });
 };
